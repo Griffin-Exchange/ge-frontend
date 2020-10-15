57debc1d8deab43c4bd3e4500d56feb29c72433a
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2020-10-15 12:00:48 +0700

    fix color StyledInfo

--- src/views/StakeXSushi/StakeXSushi.tsx
+++ src/views/StakeXSushi/StakeXSushi.tsx
@@ -105,7 +105,7 @@ const StyledCardWrapper = styled.div`
 `
 
 const StyledInfo = styled.h3`
-  color: ${(props) => props.theme.color.grey[400]};
+  color: ${(props) => props.theme.color.whiteDoff};
   font-size: 16px;
   font-weight: 400;
   margin: 0;
